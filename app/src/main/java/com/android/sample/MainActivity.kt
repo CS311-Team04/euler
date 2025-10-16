c0314512d7e6b7bb4995fefa0285566343650a25
Marc <marc.farhat@epfl.ch>
2025-10-16 05:17:52 +0200

    feat(ui):corrected main activity navigation.

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.android.sample.home.HomeScreen
+import com.android.sample.navigation.AppNav
 import com.android.sample.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { SampleAppTheme { HomeScreen() } }
+    setContent {
+      SampleAppTheme {
+        AppNav(startOnSignedIn = true)
+      }
+    }
   }
 }
