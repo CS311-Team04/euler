67898646ff04049a59838bdba3b76cbce39e32d7
Marc <marc.farhat@epfl.ch>
2025-11-04 15:37:29 +0100

    fix: fixed tests for CI

--- app/src/test/java/com/android/sample/home/HomeScreenNewFeaturesTest.kt
+++ app/src/test/java/com/android/sample/home/HomeScreenNewFeaturesTest.kt
@@ -1,6 +1,7 @@
 package com.android.sample.home
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
