9e9aaff90a131e93ebf00f11b69a6aaac969784b
hfzmoncef <moncefhafiz336@gmail.com>
2025-11-09 15:10:02 +0100

    Adding the config file of ElevenLabs

---
+++ app/src/main/java/com/android/sample/speech/ElevenLabsConfig.kt
@@ -0,0 +1,13 @@
+package com.android.sample.speech
+
+object ElevenLabsConfig {
+  /** ElevenLabs API key placeholder. */
+  const val API_KEY: String = "KEY_GOES_HERE"
+
+  /**
+   * Target voice ID.
+   *
+   * Example: "b9a9f0c3-1234-5678-90ab-cdef12345678"
+   */
+  const val VOICE_ID: String = "VOICE_ID_GOES_HERE"
+}
