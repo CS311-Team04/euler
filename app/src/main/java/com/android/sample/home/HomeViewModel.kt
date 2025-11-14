0e047765bc10b0a4220b7b8efbf917c55dd04e8b
Ziyad Mellal <ziyad.mellal@epfl.ch>
2025-11-14 09:10:26 +0100

    Firebase cloud functions now hosted on "europe-west6" for proximity and huge time gain instead of "us-central1".

--- app/src/main/java/com/android/sample/home/HomeViewModel.kt
+++ app/src/main/java/com/android/sample/home/HomeViewModel.kt
@@ -53,7 +53,7 @@ class HomeViewModel : ViewModel() {
    * BuildConfig flags.
    */
   private val functions: FirebaseFunctions by lazy {
-    FirebaseFunctions.getInstance("us-central1").apply {
+    FirebaseFunctions.getInstance("europe-west6").apply {
       if (BuildConfig.USE_FUNCTIONS_EMULATOR) {
         useEmulator(BuildConfig.FUNCTIONS_HOST, BuildConfig.FUNCTIONS_PORT)
       }
