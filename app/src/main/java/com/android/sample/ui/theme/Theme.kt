0655b8fbed213f79bb5d57de94791451afef667b
Ziyad Mellal <ziyad.mellal@epfl.ch>
2025-11-19 23:49:42 +0100

    feat: enhance theme functionality by updating bottom navigation bar color and status bar appearance settings

--- app/src/main/java/com/android/sample/ui/theme/Theme.kt
+++ app/src/main/java/com/android/sample/ui/theme/Theme.kt
@@ -77,7 +77,9 @@ fun SampleAppTheme(
     SideEffect {
       val window = (view.context as Activity).window
       window.statusBarColor = colorScheme.background.toArgb()
+      window.navigationBarColor = colorScheme.background.toArgb()
       WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+      WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
     }
   }
 
