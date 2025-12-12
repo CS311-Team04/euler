f6ed7eee54af9368b03b0d0b35aa1170f77d430b
Yassine_leb <yassinebabbah12@gmail.com>
2025-12-12 01:55:31 +0100

    fix: add tests

--- app/src/test/java/com/android/sample/home/HomeViewModelPdfDetectionTest.kt
+++ app/src/test/java/com/android/sample/home/HomeViewModelPdfDetectionTest.kt
@@ -1,12 +1,37 @@
 package com.android.sample.home
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [31])
 class HomeViewModelPdfDetectionTest {
 
-  private val vm = HomeViewModelTestHelpers.createViewModelForPdfDetection()
+  private lateinit var vm: HomeViewModel
+
+  @Before
+  fun setup() {
+    val ctx = ApplicationProvider.getApplicationContext<Context>()
+    if (FirebaseApp.getApps(ctx).isEmpty()) {
+      FirebaseApp.initializeApp(
+          ctx,
+          FirebaseOptions.Builder()
+              .setApplicationId("1:123:android:test")
+              .setProjectId("test")
+              .setApiKey("key")
+              .build())
+    }
+    vm = HomeViewModelTestHelpers.createViewModelForPdfDetection()
+  }
 
   @Test
   fun isPdfUrl_acceptsPdfWithTokenQuery() {
