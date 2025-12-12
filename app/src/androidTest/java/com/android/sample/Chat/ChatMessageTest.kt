deecd34f2e9c7742720a69ba68b8c34bc5530df7
haytam <haytam.kasbane@epfl.ch>
2025-12-12 05:33:28 +0100

    Ktfmt Format

--- app/src/androidTest/java/com/android/sample/Chat/ChatMessageTest.kt
+++ app/src/androidTest/java/com/android/sample/Chat/ChatMessageTest.kt
@@ -2,9 +2,9 @@ package com.android.sample.Chat
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
